2c470d37f037f6cf84e2d361074a7974d2fc212f
Ulisessg <ukisessg@gmail.com>
2020-12-20 01:45:58 -0600

    [ADD] JSON and url-encoded middleware

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import { port } from '../config.js';
 import { error, success } from '../network/responses.js';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', async (req, res) => {
   success(req, res, 200);

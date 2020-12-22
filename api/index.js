388a36e652c16556c6f18a90de2a21d4c73cba6f
Ulisessg <ukisessg@gmail.com>
2020-12-22 03:33:33 -0600

    [ADD] Dotenv and mongoose

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 //@ts-check
+//ENV
+import { config } from 'dotenv';
+config();
+
+//Dependencies
 import express from 'express';
-import { port } from '../config.js';
 import blog from './components/blog/network.js';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 //Middlewares
 app.use(express.json());
@@ -12,6 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 //Routes
 app.use('/api/blog', blog);
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });

114636f689a54bb1a33616693213efec8d5d6d8a
ulisessg <ukisessg@gmail.com>
2021-04-12 05:11:13 -0500

    [ADD] Email and Password required to create gist

--- api/components/gists/network.js
+++ api/components/gists/network.js
@@ -3,7 +3,7 @@ const controller = require('./index.js');
 const router = express.Router();
 const cors = require('cors');
 
-const { NODE_ENV } = process.env;
+const { NODE_ENV, EMAIL, PASSWORD } = process.env;
 
 const corsOptions = {
   origin: NODE_ENV === 'production' ? 'https://ulisessg.com/' : '*',
@@ -11,46 +11,53 @@ const corsOptions = {
 };
 
 router.post('/add-gist', cors(corsOptions), async (req, res) => {
-  const gist = {
-    subjects: req.body.subjects,
-    image: req.body.image,
-    description: req.body.description,
-    githubLink: req.body.githubLink,
-  };
-
-  controller
-    .addGist(gist)
-    .then((response) => {
-      if (
-        response === 'Error adding gist :(' ||
-        response === 'Error creating gist'
-      ) {
-        res.status(500).json({
-          error: false,
-          message: 'Error creating gist',
-        });
-      } else if (response === 'Gist created') {
-        res.status(201).json({
-          error: false,
-          message: 'Gist Created',
-        });
-      }
-    })
-
-    .catch((response) => {
-      if (NODE_ENV === 'development') {
-        console.log(response);
-        res.json({
-          error: true,
-          message: response,
-        });
-      } else if (NODE_ENV === 'production') {
-        res.status(500).json({
-          error: false,
-          message: 'Error creating gist',
-        });
-      }
+  if (req.body.email !== EMAIL || req.body.password !== PASSWORD) {
+    res.status(500).json({
+      error: true,
+      message: 'Error creating gist',
     });
+  } else {
+    const gist = {
+      subjects: req.body.subjects,
+      image: req.body.image,
+      description: req.body.description,
+      githubLink: req.body.githubLink,
+    };
+
+    controller
+      .addGist(gist)
+      .then((response) => {
+        if (
+          response === 'Error adding gist :(' ||
+          response === 'Error creating gist'
+        ) {
+          res.status(500).json({
+            error: false,
+            message: 'Error creating gist',
+          });
+        } else if (response === 'Gist created') {
+          res.status(201).json({
+            error: false,
+            message: 'Gist Created',
+          });
+        }
+      })
+
+      .catch((response) => {
+        if (NODE_ENV === 'development') {
+          console.log(response);
+          res.json({
+            error: true,
+            message: response,
+          });
+        } else if (NODE_ENV === 'production') {
+          res.status(500).json({
+            error: false,
+            message: 'Error creating gist',
+          });
+        }
+      });
+  }
 });
 
 router.get('/', async (req, res) => {

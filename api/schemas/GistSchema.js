15e02dcaed4cf7f9d17059484b916d2fcc6f8186
ulisessg <ukisessg@gmail.com>
2021-04-12 17:45:11 -0500

    [UPDATE] Gist schema updated

--- api/schemas/GistSchema.js
+++ api/schemas/GistSchema.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const AddGistSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
   subjects: {
     type: Array,
     required: true,
